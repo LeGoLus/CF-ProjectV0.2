6c0d5193e04f51123e0b0187b21c0d0462edd7db
GainHunter <gain_hunter@hotmail.com>
2023-07-30 22:23:57 +0700

    FrontEnd-V0.05-Integrated-with-BackEnd

--- src/pages/actionbar.tsx
+++ src/pages/actionbar.tsx
@@ -8,14 +8,14 @@ export default function ActionBar() {
   return (
     <Card className="bg-white w-screen p-4 drop-shadow-md">
       <div className="grid grid-cols-2 gap-2 ">
-        <div><h1 className="font-semibold text-2xl">Welcome (V0.034 Delta)</h1><h2>Have a good day!</h2></div>
+        <div><h1 className="font-semibold text-2xl">Welcome (V0.04 )</h1><h2>Have a good day!</h2></div>
         <div className="grid grid-cols-3 gap-3 ">   
         <div></div>
         <button className=" px-4 py-1  tracking-wide text-white font-semibold transition-colors duration-200 transform bg-green-700 rounded hover:bg-gray-500 focus:outline-none focus:bg-gray-500" >
             <Link href="/createDocument"> + Create Document</Link>
         </button>
         <button className=" px-4 py-1  tracking-wide text-white font-semibold transition-colors transform bg-gray-700 rounded hover:bg-gray-500 focus:outline-none focus:bg-gray-500" >
-            <Link href="/createProject"> + Create Project</Link>
+            <Link href="/project-creation-page"> + Create Project</Link>
         </button>
         </div>
         <div>
